7d15104ef51fe445dda6d515e68850180e0a9328
OsmanXwiinder <osmansaifi30@gmail.com>
2025-07-27 18:51:51 +0530

    pages complete now functionalatiy

--- frontend-Tm/src/components/UserCard.jsx
+++ frontend-Tm/src/components/UserCard.jsx
@@ -1,9 +1,15 @@
-
+import { Link } from "react-router-dom"
 
 export function UserCard({name}){
 
     return <div className="flex flex-row justify-between items-center w-md m-10 border border-2xl p-3 rounded-xl">
-        {name}
-        <button className="text-white bg-purple-500 p-2 rounded-xl">Send Money</button>
+         <div className="flex flex-row items-center gap-2">
+            <img
+      className="w-8 h-8 rounded-full object-cover object-center"
+      src="src/assets/profile.jpg"
+      alt="nature image"/><span className="text-lg">{name}</span>
+         </div>
+        <Link className="text-white bg-purple-500 p-2 rounded-xl" to={"/sendmoney"}>Send Money</Link>
+        
     </div>
 }
--- frontend-Tm/src/pages/SendMoney.jsx
+++ frontend-Tm/src/pages/SendMoney.jsx
@@ -1,7 +1,24 @@
 
 export function SendMoney(){
 
-    return <>
-        send money
-    </>
+    return <div className="">   
+        <div className="h-screen justify-center items-center flex flex-col  gap-20">
+<span className="flex justify-center items-center text-3xl font-bold">Send Money</span>
+            <div className="p-20  border-black border-2 rounded-2xl">
+                <div className="flex flex-col gap-9">
+                <div className="flex flex-row items-center gap-5 border border-black rounded-2xl p-1 w-50 hover:border-purple-600 border-2">
+            <img className="w-8 h-8 rounded-full object-cover object-center ml-2" src="src/assets/profile.jpg" alt="nature image"/><span className="text-lg">Roodius</span>
+             </div>
+             <div>
+                <span className="text-gray-600">Amount in(Rs)</span>
+             </div>
+            </div>
+            <div className="flex flex-col gap-9">
+                <input className="focus:outline-none" type="text" placeholder="Enter Amount"/>
+                <button className="w-80 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4  rounded-lg placeholder:font-semibold">Initiate Transfer</button>
+            </div>
+            
+            </div>
+        </div>
+    </div>
 }
